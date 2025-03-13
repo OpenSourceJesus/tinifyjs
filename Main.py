143c908add6b2f207a49071ab9ee82f271ab7068
Gilead Cosman <gileadcosman@gmail.com>
2025-03-12 21:13:45 -0700

    Replaced many more members with their corresponding first and last letters

--- Main.py
+++ Main.py
@@ -1,14 +1,15 @@
-import sys
+import sys, os
 from StringExtensions import *
 
 TEXT_INDICATOR = '-t='
 INPUT_INDICATOR = '-i='
 OUTPUT_INDICATOR = '-o='
-text = ''
-OUTPUT_PREFIX = '''m={}
-for(o of [Element,Node]){for(n of Object.keys(o.prototype)){a=n[0]+n[n.length-1]
+REMAP_CODE = '''m={}
+for(o of [Element,Node,String]){for(n of Object.getOwnPropertyNames(o.prototype)){a=n[0]+n[n.length-1]
 try{if(!(a in m))o.prototype[a]=o.prototype[n]
 m[a]=1}catch(e){}}}'''
+_thisDir = os.path.split(os.path.abspath(__file__))[0]
+text = ''
 output = ''
 outputPath = None
 
@@ -26,9 +27,15 @@ for i, char in enumerate(text):
 		indicesOfEnclosingString = IndicesOfEnclosingStringQuotes(text, i)
 	if (indicesOfEnclosingString and i < indicesOfEnclosingString[1]) or (char not in '\t\n' and (i == 0 or (i > 0 and (char != ' ' or text[i - 1] != ' ')))):
 		output += char
-outputWithPrefix = OUTPUT_PREFIX + output.replace('setAttribute', 'se').replace('getAttribute', 'ge').replace('appendChild', 'ad')
-if len(output) > len(outputWithPrefix):
-	output = outputWithPrefix
+remappedOutput = output
+memberRemap = open(os.path.join(_thisDir, 'MemberRemap'), 'r').read()
+for line in memberRemap.split('\n'):
+	clauses = line.split()
+	remappedOutput = remappedOutput.replace('.' + clauses[0], '.' + clauses[1])
+	remappedOutput = remappedOutput.replace('[' + clauses[0] + ']', '[' + clauses[1] + ']')
+remappedOutput = REMAP_CODE + remappedOutput
+if len(output) > len(remappedOutput):
+	output = remappedOutput
 if outputPath:
 	open(outputPath, 'w').write(output)
 else:
