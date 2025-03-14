e67f8464d42820819b2e8595855fd1cef5062b30
Gilead Cosman <gileadcosman@gmail.com>
2025-03-14 12:07:17 -0700

    More work on removing for loops and adding them back later

--- Main.py
+++ Main.py
@@ -15,6 +15,7 @@ e--}try{p[a]=p[n]
 m[a]=1}catch(e){}}}'''
 OKAY_NAME_CHARS = list(string.ascii_letters + '$_')
 OKAY_NAME_CHARS.remove('m')
+OKAY_NAME_CHARS.remove('o')
 WHITESPACE_EQUIVALENT = string.whitespace + ';'
 MEMBER_REMAP = {}
 _thisDir = os.path.split(os.path.abspath(__file__))[0]
@@ -29,8 +30,8 @@ unusedNames = []
 unusedNames.extend(OKAY_NAME_CHARS)
 mangledMembers = {}
 usedNames = []
-currentClause = ''
-prevClause = ''
+currentWord = ''
+prevWord = ''
 indicesOfEnclosingStringStartEnd = None
 
 for arg in sys.argv:
@@ -45,31 +46,36 @@ for i, char in enumerate(text):
 	if not indicesOfEnclosingStringStartEnd:
 		indicesOfEnclosingStringStartEnd = IndicesOfEnclosingStringStartEnd(text, i)
 	if not indicesOfEnclosingStringStartEnd or i > indicesOfEnclosingStringStartEnd[1]:
-		endClauseChars = string.punctuation.replace('_', '').replace('$', '') + WHITESPACE_EQUIVALENT
+		endWordChars = string.punctuation.replace('_', '').replace('$', '') + WHITESPACE_EQUIVALENT
 		for digit in string.digits:
-			if currentClause.startswith(digit):
-				endClauseChars += string.digits
+			if currentWord.startswith(digit):
+				endWordChars += string.digits
 				break
-		if char in endClauseChars:
-			if currentClause in mangledMembers:
-				output = output[: -len(currentClause)] + mangledMembers[currentClause]
-			if prevClause in ['let', 'var', 'function'] and currentClause not in mangledMembers and currentClause not in usedNames:
-				if len(currentClause) > 1:
+		if char in endWordChars:
+			if currentWord in mangledMembers:
+				output = output[: -len(currentWord)] + mangledMembers[currentWord]
+			if prevWord in ['let', 'var', 'function'] and currentWord not in mangledMembers and currentWord not in usedNames:
+				if len(currentWord) > 1:
 					while len(unusedNames) == 0:
 						unusedName = random.choice(OKAY_NAME_CHARS) + random.choice(OKAY_NAME_CHARS)
-						if unusedName not in MEMBER_REMAP.values() and unusedName not in mangledMembers and unusedName not in usedNames and unusedName not in ['if', 'do']:
+						if unusedName not in MEMBER_REMAP.values() and unusedName not in mangledMembers and unusedName not in usedNames and unusedName not in ['if', 'do', 'of', 'in']:
 							unusedNames.append(unusedName)
-					mangledMembers[currentClause] = unusedNames.pop(random.randint(0, len(unusedNames) - 1))
-					output = output[: -len(currentClause)] + mangledMembers[currentClause]
+					mangledMembers[currentWord] = unusedNames.pop(random.randint(0, len(unusedNames) - 1))
+					output = output[: -len(currentWord)] + mangledMembers[currentWord]
 				else:
-					usedNames.append(currentClause)
-			elif prevClause == 'for':
-				pass
-			prevClause = currentClause
-			currentClause = ''
+					usedNames.append(currentWord)
+			elif prevWord == 'for':
+				prevWordIndex = text.rfind(prevWord, i)
+				leftParenthesisIndex = text.find('(', prevWordIndex)
+				rightParenthesisIndex = IndexOfMatchingRightChar(text, '(', ')', leftParenthesisIndex)
+				forClause = text[prevWordIndex : rightParenthesisIndex + 1]
+				if ' of ' in forClause:
+					print('YAY', forClause)
+			prevWord = currentWord
+			currentWord = ''
 		else:
-			currentClause += char
-	if (indicesOfEnclosingStringStartEnd and i < indicesOfEnclosingStringStartEnd[1]) or (char in WHITESPACE_EQUIVALENT and ((text[i - 1] not in WHITESPACE_EQUIVALENT + string.punctuation) or prevClause in ['return', 'let', 'var', 'function', 'else', 'of'])) or char in string.ascii_letters + string.digits + string.punctuation:
+			currentWord += char
+	if (indicesOfEnclosingStringStartEnd and i < indicesOfEnclosingStringStartEnd[1]) or (char in WHITESPACE_EQUIVALENT + string.punctuation and (text[i - 1] not in WHITESPACE_EQUIVALENT + string.punctuation or prevWord in ['return', 'let', 'var', 'function', 'if', 'else', 'of', 'in', 'while'])) or char in string.ascii_letters + string.digits + string.punctuation:
 		output += char
 remappedOutput = output
 for key, value in MEMBER_REMAP.items():
@@ -90,10 +96,10 @@ b=await r.blob()
 s=b.stream().pipeThrough(d)
 o=await new Response(s).blob()
 return await o.text()}
-u("%s",1).then((j)=>{
-eval(j)})''' %jsBytes
+u("%s",1).then((j)=>{eval(j)})''' %jsBytes
 if len(output) > len(outputWithDecompression):
 	output = outputWithDecompression
 open(outputPath, 'w').write(output)
 cmd = ['gzip', '--keep', '--force', '--verbose', '--best', outputPath]
 subprocess.check_call(cmd)
+print(output)
