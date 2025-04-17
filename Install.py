52977c6801ca74710faa2909a68dbb9b32d82589
Gilead Cosman <gileadcosman@gmail.com>
2025-04-17 05:43:46 -0700

    Made install script install working versions of tree-sitter

--- Install.py
+++ Install.py
@@ -1,4 +1,4 @@
 import os
 
-os.system('''pip install tree-sitter --break-system-packages
-pip install tree-sitter-javascript --break-system-packages''')
+os.system('''pip install tree-sitter==0.23.0 --break-system-packages
+pip install tree-sitter-javascript==0.23.0 --break-system-packages''')
